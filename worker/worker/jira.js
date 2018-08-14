3532bfc2b461a3042e79c4d363bb24bffe46db1a
Pacôme Françon <pacome.francon@epitech.eu>
2018-08-14 17:41:43 +0200

    adding get_project_list() + project list to choose

--- worker/worker/jira.js
+++ worker/worker/jira.js
@@ -1,14 +1,39 @@
 var axios = require('axios');
 
 var config = {
-	headers: {
-		Authorization: 'Basic ZGFtaWVuQHpldGFwdXNoLmNvbTpqNktzVFl0dHdud2l1WWJRb0NEV0E1NUY=',
-	}
+	auth: {
+		username: 'damien@zetapush.com',
+		password: 'j6KsTYttwnwiuYbQoCDWA55F'
+	},
 };
+
+var project = ['BiOSENCY Tracker'];
+
+async function get_project_list()
+{
+	var url = [];
+	var res = await axios.get('https://zetapush.atlassian.net/rest/api/2/project/', config);
+
+	for (var i = 0; i < res.data.length; i++) {
+		project.find((name) => {
+			if (name == res.data[i].name)
+				url.push(res.data[i].self);
+		});
+	}
+	return url;
+}
 
 module.exports = async function()
 {
-	var res = await axios.get('https://zetapush.atlassian.net/rest/api/2/project/', config)
+	var data = [];
+	var project_list = await get_project_list();
 
-	return res.data;
+	console.log(project_list);
+	for (var i = 0; i < project_list.length; i++) {
+		var res = await axios.get(project_list[i], config);
+
+		data.push(res.data);
+	}
+
+	return data;
 }
