eb9bd27542384b8d9d1a326937da329a5e76ebab
Pacôme Françon <pacome.francon@epitech.eu>
2018-08-16 18:57:10 +0200

    try to understand jira api

--- worker/worker/jira.js
+++ worker/worker/jira.js
@@ -7,33 +7,61 @@ var config = {
 	},
 };
 
-var project = ['BiOSENCY Tracker'];
+var project = ['BiOSENCY Tracker', 'BiOSENCY'];
+var api = 'https://zetapush.atlassian.net/rest/api/2';
 
-async function get_project_list()
+async function get_project_key_list()
 {
-	var url = [];
-	var res = await axios.get('https://zetapush.atlassian.net/rest/api/2/project/', config);
+	var key = [];
+	var res = await axios.get(`${api}/project/`, config);
 
 	for (var i = 0; i < res.data.length; i++) {
 		project.find((name) => {
 			if (name == res.data[i].name)
-				url.push(res.data[i].self);
+				key.push(res.data[i].key);
 		});
 	}
-	return url;
+	return key;
+}
+
+function filter_data(res) {
+	res.data.issues.forEach((elem) => {
+		for (var key in elem.fields)
+			if (key.match(/customfield_/))
+				delete elem.fields[key];
+	});
+}
+
+async function get_issues_list(project_key)
+{
+	var issues = [];
+	var res = await axios.get(`${api}/search?jql=project=${project_key}&maxResults=1`, config);
+	var max = res.data.total;
+
+	filter_data(res);
+	issues.push(res.data.issues[0]);
+	for (var i = 0; i < max; i += 100) {
+		res = await axios.get(`${api}/search?jql=project=${project_key}&startAt=${i + 1}&maxResults=100`, config);
+		filter_data(res);
+		issues = issues.concat(res.data.issues);
+	}
+	return issues.filter((issue) => issue.fields.status.name != 'Delivered');
 }
 
 module.exports = async function()
 {
 	var data = [];
-	var project_list = await get_project_list();
+	var keys = await get_project_key_list();
 
-	console.log(project_list);
-	for (var i = 0; i < project_list.length; i++) {
-		var res = await axios.get(project_list[i], config);
+	for (var i = 0; i < keys.length; i++) {
+		var issues = await get_issues_list(keys[i]);
 
-		data.push(res.data);
+		data.push({
+			project: project[i],
+			key: keys[i],
+			issues: issues
+		});
 	}
-
-	return data;
+	return data[0];
 }
+
