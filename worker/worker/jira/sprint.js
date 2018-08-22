65b2c4ff9855c3a628d17723bec968c3174df48a
Pacôme Françon <pacome.francon@epitech.eu>
2018-08-22 09:30:26 +0200

    add get_crrent_sprint => jira.sprint

--- worker/worker/jira/sprint.js
+++ worker/worker/jira/sprint.js
@@ -23,14 +23,36 @@ async function get_board_list(project_list, config)
 	return list;
 }
 
+async function get_current_sprint(board_id, config)
+{
+	var sprint = {};
+	var res = await axios.get(`${api}/board/${board_id}/sprint?state=active`, config)
+
+	res = res.data.values[0];
+	sprint = {
+		id: res.id,
+		name: res.name,
+		start: utils.parse_time(res.startDate).slice(0, -9),
+		end: utils.parse_time(res.endDate).slice(0, -9)
+	};
+
+	return sprint;
+}
+
 module.exports = async function()
 {
 	var data = [];
 	const config = utils.get_config('jira');
 	const boards_id = await get_board_list(config.sprint.project_list, config.http);
 
-	console.log(boards_id)
-	// for (var i = 0; i < boards.length; i++)
-	// 	var sprint
+	for (var i = 0; i < boards_id.length; i++) {
+		var sprint = await get_current_sprint(boards_id[i], config.http)
+
+		data.push({
+			project: config.sprint.project_list[i],
+			id: boards_id[i],
+			sprint: sprint
+		});
+	}
 	return data;
 }
