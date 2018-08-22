728a5b73384143ac627165b491e22c4a23fd2981
Pacôme Françon <pacome.francon@epitech.eu>
2018-08-22 14:50:52 +0200

    change message error + group jira project configuration in zetarc

--- worker/worker/jira/sprint.js
+++ worker/worker/jira/sprint.js
@@ -18,34 +18,33 @@ async function get_board_list(project_list, config)
 	res = res.data.values;
 	for (var i = 0; i < project_list.length; i++)
 		for (var j = 0; j < res.length; j++)
-			if (res[j].location.name === project_list[i])
+			if (res[j].location.name === `${project_list[i].name} (${project_list[i].key})`)
 				list.push(res[j].id);
 	return list;
 }
 
-async function get_issues_list(project_key, config)
+async function get_issues_list(project_config, config)
 {
-	const api_tmp = 'https://zetapush.atlassian.net/rest/api/2';
+	const api_url = `https://zetapush.atlassian.net/rest/api/2/search?jql=project=${project_config.key}`;
 	var issues = [];
-	console.log(`${api_tmp}/search?jql=project=${project_key}&maxResults=1`);
-	var res = await axios.get(`${api_tmp}/search?jql=project=${project_key}&maxResults=1`, config).catch((err) => {
+	var res = await axios.get(`${api_url}&maxResults=1`, config).catch((err) => {
 		if (err.response.status != 200) {
 			console.log(err.response.status, err.response.statusText);
-			console.log('The authenticated account is not allowed to see that.');
+			console.log(`The authenticated account is not allowed to see\n\t => ${project_config.name}`);
 			process.exit(1);
 		}
 	});
 	var max = res.data.total;
 
 	for (var i = 0; i < max; i += 100) {
-		res = await axios.get(`${api_tmp}/search?jql=project=${project_key}&startAt=${i}&maxResults=100`, config);
-		res.data.issues = res.data.issues.filter(issue => issue.fields.status.name == 'Terminé');
+		res = await axios.get(`${api_url}&startAt=${i}&maxResults=100`, config);
+		res.data.issues = res.data.issues.filter(issue => issue.fields.status.name !== project_config.close_state);
 		issues = issues.concat(utils.filter_data(res.data.issues));
 	}
 	return issues;
 }
 
-async function get_current_sprint(board_name, board_id, config)
+async function get_current_sprint(project_config, board_id, config)
 {
 	var sprint = {};
 	var res = await axios.get(`${api}/board/${board_id}/sprint?state=active`, config)
@@ -56,9 +55,8 @@ async function get_current_sprint(board_name, board_id, config)
 		name: res.name,
 		start: utils.parse_time(res.startDate).slice(0, -9),
 		end: utils.parse_time(res.endDate).slice(0, -9),
-		issues: await get_issues_list(board_name, config)
+		issues: await get_issues_list(project_config, config)
 	};
-
 	return sprint;
 }
 
@@ -66,13 +64,19 @@ module.exports = async function()
 {
 	var data = [];
 	const config = utils.get_config('jira');
-	const boards_id = await get_board_list(config.sprint.project_list, config.http);
+	const projects_config = config.sprint.project_list;
+	const boards_id = await get_board_list(projects_config, config.http);
 
+	if (boards_id.length == 0) {
+		console.log('No valid projects were found =>');
+		console.log('jira: {\n\tproject_list: {name || key}\n} => .zetarc');
+		process.exit(1);
+	}
 	for (var i = 0; i < boards_id.length; i++) {
-		var sprint = await get_current_sprint(config.sprint.project_list[i].split(' ')[0], boards_id[i], config.http)
+		var sprint = await get_current_sprint(projects_config[i], boards_id[i], config.http)
 
 		data.push({
-			project: config.sprint.project_list[i],
+			project: projects_config[i].name,
 			id: boards_id[i],
 			sprint: sprint
 		});
