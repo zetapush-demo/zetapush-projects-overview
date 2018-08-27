42f1049fc33717e98d64f74303f57d201044de5a
Pacôme Françon <pacome.francon@epitech.eu>
2018-08-27 11:13:29 +0200

    jenkins : add handling => build in progress

--- worker/worker/jenkins.js
+++ worker/worker/jenkins.js
@@ -1,6 +1,8 @@
 const axios = require('axios');
 const utils = require('./utils');
 
+const jenkins_assets = 'https://raw.githubusercontent.com/jenkinsci/jenkins/master/war/src/main/webapp/images/48x48/';
+
 async function get_repo_list(jenkins_url)
 {
 	var url = [];
@@ -13,13 +15,13 @@ async function get_repo_list(jenkins_url)
 	});
 
 	for (var i = 0; i < res.data.jobs.length; i++)
-		url.push(`${res.data.jobs[i].url}/api/json`);
+		url.push(`${res.data.jobs[i].url}api/json`);
 	return url;
 }
 
 async function get_timestamp_last_build(build_url)
 {
-	const res = await axios.get(build_url + 'api/json');
+	const res = await axios.get(`${build_url}api/json`);
 
 	return utils.parse_time(res.data.timestamp);
 }
@@ -31,19 +33,28 @@ async function get_branch_array(branch_url_array)
 	if (!branch_url_array)
 		return '0 branch';
 	for (var i = 0; i < branch_url_array.length; i++) {
-		const res = await axios.get(`${branch_url_array[i].url}/api/json`);
+		const res = await axios.get(`${branch_url_array[i].url}api/json`);
+		var new_branch = {};
 
-		branchs.push({
-			name: res.data.displayName,
-			last_build: {
+		new_branch.name = res.data.displayName;
+		new_branch.time = await get_timestamp_last_build(res.data.lastBuild.url);
+		new_branch.branch_url = res.data.url;
+		if (!res.data.healthReport.length) {
+			new_branch.last_build = {
+				url: res.data.lastBuild.url,
+				icon: `${jenkins_assets}nobuilt_anime.gif`,
+				description: 'Build in progress',
+				in_progress: true
+			};
+		} else {
+			new_branch.last_build = {
 				description: res.data.healthReport[0].description,
 				url: res.data.lastBuild.url,
-				icon: 'https://raw.githubusercontent.com/jenkinsci/jenkins/master/war/src/main/webapp/images/48x48/' + res.data.healthReport[0].iconUrl,
+				icon: `${jenkins_assets}${res.data.healthReport[0].iconUrl}`,
 				score: res.data.healthReport[0].score,
-			},
-			time: await get_timestamp_last_build(res.data.lastBuild.url),
-			branch_url: res.data.url
-		});
+			};
+		}
+		branchs.push(new_branch);
 	}
 	return branchs;
 }
@@ -52,7 +63,7 @@ module.exports = async function()
 {
 	var data = [];
 	const config = utils.get_config('jenkins');
-	const jenkins_url = `${config.url}/api/json`;
+	const jenkins_url = `${config.url}api/json`;
 	var repo_list = await get_repo_list(jenkins_url);
 
 	for (var i = 0; i < repo_list.length; i++) {
