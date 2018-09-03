dc2e2a48734bfb23c7607b93f59a94d577f1457c
Pacôme Françon <pacome.francon@epitech.eu>
2018-09-03 10:11:12 +0200

    bugfix : multiple same popup on old pull_request/issues

--- webapp/src/app/zetapush-project/github/github.component.ts
+++ webapp/src/app/zetapush-project/github/github.component.ts
@@ -33,27 +33,33 @@ export class GithubComponent implements OnInit {
 		});
 	}
 
-	popup_on_new_data(delay) {
+	popup_on_new_data(delay: number[]) {
 		const now = new Date().valueOf();
 		var popup_data;
 
-		function get_last_data(tab) {
+		function get_last_data(tab, delay_id) {
 			if (!tab)
 				return null;
 			for (var i = 0; i < tab.length; i++) {
-				const gap = now - delay;
+				const gap = now - delay[delay_id];
 
 				if (new Date(tab[i].created).valueOf() > gap)
 					return tab[i];
 			}
 			return null;
 		}
-		popup_data = get_last_data(this.data.issues);
-		if (popup_data !== null)
-			this.openDialog(popup_data, 'New Issue !!');
-		popup_data = get_last_data(this.data.pull_request);
-		if (popup_data !== null)
-			this.openDialog(popup_data, 'New Pull request !!');
+		for (var i = 0; i < delay.length; i++) {
+			popup_data = get_last_data(this.data.issues, i);
+			if (popup_data) {
+				this.openDialog(popup_data, 'New Issue !!');
+				break;
+			}
+			popup_data = get_last_data(this.data.pull_request, i);
+			if (popup_data) {
+				this.openDialog(popup_data, 'New Pull request !!');
+				break;
+			}
+		}
 	}
 
 	filter_data_by(value, field, subfield) {
@@ -103,10 +109,12 @@ export class GithubComponent implements OnInit {
 			['login', 'name']
 		);
 		console.log(this.data);
-		this.popup_on_new_data(1000 * 60 * 15); // 15 minutes
-		this.popup_on_new_data(1000 * 60 * 60 * 24); // 24 hours
-		this.popup_on_new_data(1000 * 60 * 60 * 24 * 3); // 3 days
-		this.popup_on_new_data(1000 * 60 * 60 * 24 * 7 * 1); // 1 week
+		this.popup_on_new_data([
+			1000 * 60 * 15, // 15 minutes
+			1000 * 60 * 60 * 24, // 24 hours
+			1000 * 60 * 60 * 24 * 3, // 3 days
+			1000 * 60 * 60 * 24 * 7 * 1 // 1 week
+		]);
 	}
 
 	async ngOnInit() {
