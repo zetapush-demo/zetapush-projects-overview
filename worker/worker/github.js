2267899bdbf5d2fc8c661206c6a3fd7cffeaf931
Pacôme Françon <pacome.francon@epitech.eu>
2018-09-05 14:28:26 +0200

    github handle multiple repository + fix bug on >30 issues

--- worker/worker/github.js
+++ worker/worker/github.js
@@ -1,11 +1,12 @@
-var axios = require('axios');
-var { get_config, parse_time, obj_tab_filter, get_good_color } = require('./utils');
+const axios = require('axios');
+const { get_config, parse_time, obj_tab_filter, get_good_color } = require('./utils');
 
 const api_url = 'https://api.github.com/repos/zetapush';
 
-async function get_repo_name(config)
+async function get_repo_list(repos, config)
 {
-	const res = await axios.get('https://api.github.com/orgs/zetapush/repos', config.http).catch((err) => {
+	var repo_list = [];
+	const res = await axios.get('https://api.github.com/orgs/zetapush/repos', config).catch(err => {
 		if (err.response.status != 200) {
 			console.error(err.response.status, err.response.statusText);
 			console.error('Bad credentials => .zetarc =>');
@@ -14,96 +15,95 @@ async function get_repo_name(config)
 		}
 	});
 
-	for (var i = 0; i < res.data.length; i++)
-		if (res.data[i].name === config.repo && res.data[i].open_issues)
-			return res.data[i].name;
-	return null;
+	for (var i = 0; i < repos.length; i++)
+		for (var j = 0; j < res.data.length; j++) {
+			if (res.data[j].name === repos[i] && res.data[j].open_issues) {
+				repo_list.push({
+					name: res.data[j].name,
+					issues_nb: res.data[j].open_issues
+				});
+				break;
+			}
+			if (j === res.data.length - 1) {
+				console.error(`Something bad in .zetarc, or this github repository doesn't exist =>\github: { \n\t sprint: {`);
+				console.error(`\t\t repos: [{\n\t\t\t name: "${repos[i]}"`);
+				console.error(`\t\t}]\n\t}\n}`);
+				process.exit(1);
+			}
+		}
+	return repo_list;
 }
 
 async function get_tag(config, repo_name)
 {
 	const res = await axios.get(`${api_url}/${repo_name}/tags`, config.http);
 
-	return res.data[0].name || '';
+	if (!res.data.length)
+		return '';
+	return res.data[0].name;
 }
 
-async function get_issues(config, repo_name)
+function filter_data(issue)
 {
-	const res = await axios.get(`${api_url}/${repo_name}/issues`, config.http);
-	var data = res.data;
+	issue = {
+		name: issue.title,
+		id: issue.number,
+		url: issue.html_url,
+		user: {
+			name: issue.user.login,
+			avatar: issue.user.avatar_url,
+			url: issue.user.html_url
+		},
+		created: parse_time(issue.created_at),
+		labels: get_good_color(obj_tab_filter(issue.labels, ['name', 'color'])),
+		body: issue.body,
+		assignees: obj_tab_filter(issue.requested_reviewers || issue.assignees, ['login', 'avatar_url']),
+		head: issue.head && issue.head.ref,
+		base: issue.base && issue.base.ref
+	};
+	for (var tmp in issue)
+		if (!issue[tmp] || issue[tmp].length === 0 || (Object.keys(issue[tmp]).length === 0 && issue[tmp].constructor === Object))
+			delete issue[tmp];
+	return issue;
+}
+
+async function get_data(config, api_search_field, repo_name, issues_nb)
+{
+	var res;
 	var issues = [];
 
-	for (var i = 0; i < data.length; i++) {
-		if (data[i].pull_request)
-			continue;
-		data[i] = {
-			name: data[i].title,
-			id: data[i].number,
-			url: data[i].html_url,
-			user: {
-				name: data[i].user.login,
-				avatar: data[i].user.avatar_url,
-				url: data[i].user.html_url
-			},
-			created: parse_time(data[i].created_at),
-			labels: get_good_color(obj_tab_filter(data[i].labels, ["name", "color"])),
-			body: data[i].body,
-			assignees: obj_tab_filter(data[i].assignees, ["login", "avatar_url"])
-		};
-		issues.push(data[i]);
+	for (var i = 0; i < issues_nb; i += 30) {
+		res = await axios.get(`${api_url}/${repo_name}/${api_search_field}?page=${i / 30 + 1}`, config.http);
+		if (!res.data.length)
+			break;
+		for (var j = 0; j < res.data.length; j++) {
+			if (res.data[j].pull_request)
+				continue;
+			issues.push(filter_data(res.data[j]));
+		}
 	}
 	return issues;
-}
-
-async function get_pull_request(config, repo_name)
-{
-	const res = await axios.get(`${api_url}/${repo_name}/pulls`, config.http);
-	var data = res.data;
-	var pull_request = [];
-
-	for (var i = 0; i < data.length; i++) {
-		data[i] = {
-			name: data[i].title,
-			id: data[i].number,
-			url: data[i].html_url,
-			user: {
-				name: data[i].user.login,
-				avatar: data[i].user.avatar_url,
-				url: data[i].user.html_url
-			},
-			created: parse_time(data[i].created_at),
-			labels: get_good_color(obj_tab_filter(data[i].labels, ["name", "color"])),
-			body: data[i].body,
-			assignees: obj_tab_filter(data[i].requested_reviewers, ["login", "avatar_url"]),
-			head: data[i].head.ref,
-			base: data[i].base.ref
-		};
-		pull_request.push(data[i]);
-	}
-	return pull_request;
 }
 
 module.exports = async function()
 {
 	const config = get_config('github');
-	const repo_name = await get_repo_name(config);
-	var data = {};
+	const repo_list = await get_repo_list(config.repos, config.http);
+	var data = [];
 
-	if (!repo_name) {
-		console.error(`Repository "${config.repo}" not found.`);
-		process.exit(1);
+	for (var i = 0; i < repo_list.length; i++) {
+		await Promise.all([
+			get_tag(config, repo_list[i].name, repo_list[i].issues_nb),
+			get_data(config, 'issues', repo_list[i].name, repo_list[i].issues_nb),
+			get_data(config, 'pulls', repo_list[i].name, repo_list[i].issues_nb)
+		]).then((res) => {
+			data.push({
+				repo: repo_list[i].name,
+				tag: res[0],
+				issues: res[1],
+				pull_request: res[2]
+			});
+		});
 	}
-	await Promise.all([
-		get_tag(config, repo_name),
-		get_issues(config, repo_name),
-		get_pull_request(config, repo_name)
-	]).then((res) => {
-		data = {
-			repo: repo_name,
-			tag: res[0],
-			issues: res[1],
-			pull_request: res[2]
-		};
-	});
 	return data;
 }
