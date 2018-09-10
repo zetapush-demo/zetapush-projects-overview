e6b2d6eeb79edde9ef00abb5c06f17dfe8ccb988
Pacôme Françon <pacome.francon@epitech.eu>
2018-09-10 15:18:32 +0200

    fix router tabs bug + add tabs icon <img>

--- webapp/src/app/zetapush-project/zetapush-project.component.ts
+++ webapp/src/app/zetapush-project/zetapush-project.component.ts
@@ -7,7 +7,23 @@ import { Component } from '@angular/core';
 })
 export class ZetapushProjectComponent {
 
-	navlinks = ['github', 'jenkins', 'jira'];
+	navlinks = [
+		{
+			path: '/github',
+			name: 'github',
+			icon: 'https://assets-cdn.github.com/images/modules/logos_page/GitHub-Mark.png'
+		},
+		{
+			path: '/jenkins',
+			name: 'jenkins',
+			icon: 'https://raw.githubusercontent.com/jenkinsci/jenkins/master/war/src/main/webapp/images/jenkins-redbg.png'
+		},
+		{
+			path: '/jira',
+			name: 'jira',
+			icon: 'https://wac-cdn.atlassian.com/dam/jcr:37f03d61-2cfc-4862-b536-ca6d200e7cf8/Jira%20Software-icon-blue.svg?cdnVersion=jw'
+		},
+	];
 
 	constructor() {}
 }
