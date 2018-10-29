4fd7a7171e3e748ac1b267c57ec9c160894ad1e7
Pacôme Françon <pacome.francon@epitech.eu>
2018-10-29 13:55:52 +0100

    add small echo in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,7 @@
 # This script is used to automatically deploy a worker with Angular front
 
 cd webapp
+echo "Building Angular webapp..."
 ng build --prod # build webapp angular
 cd ../worker/front
 ln -vfs ../../webapp/dist/zetapush-projects-overview/* ./ # create new symbolic link (and remove existing symbolic link)
