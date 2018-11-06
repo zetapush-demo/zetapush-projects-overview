f5289d50f53f4e03564bd6d0c9f4fe22ec6ed4d4
Pacôme Françon <pacome.francon@epitech.eu>
2018-11-06 11:16:49 +0100

    [fix] remove trailing broken symbolic link

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,7 @@ cd webapp
 echo "Building Angular webapp..."
 ng build --prod # build webapp angular
 cd ../worker/front
-ln -vfs ../../webapp/dist/zetapush-projects-overview/* ./ # create new symbolic link (and remove existing symbolic link)
+rm *
+ln -vs ../../webapp/dist/zetapush-projects-overview/* ./ # create new symbolic link (and remove existing symbolic link)
 cd ..
 npm run deploy
