96792afc03f720d124845ff2f1520675f5561a63
Pacôme Françon <pacome.francon@epitech.eu>
2018-11-07 12:16:27 +0100

    move bitrix : webapp -> worker

--- worker/worker/bitrix.js
+++ worker/worker/bitrix.js
@@ -1,14 +1,15 @@
 const axios = require('axios');
+const { get_config } = require('./utils');
 
 var exports = module.exports = {};
 
 async function get_user_id(config, email)
 {
 	const url = `${config.url}/${config.id}/${config.token}/user.get`;
-	var res = await axios.get(url, config);
+	var res = await axios.get(url).catch(err => console.error(err));
 
-	if (res && res.result) {
-		res = res.result.find(x => x.EMAIL === email);
+	if (res && res.data && res.data.result) {
+		res = res.data.result.find(x => x.EMAIL === email);
 		return res && res.ID;
 	}
 }
@@ -17,28 +18,28 @@ async function get_channel_id(config, channel)
 {
 	const url = `${config.url}/${config.id}/${config.token}/im.search.chat.list`
 	const data = `FIND="${channel}"`;
-	const res = await axios.post(url, data, config);
+	const res = await axios.post(url, data).catch(err => console.error(err));
 
-	if (res && res.result && res.result.length)
-		return res.result[0].id;
+	if (res && res.data && res.data.result && res.result.length)
+		return res.data.result[0].id;
 }
 
 exports.send_message_channel = async function send_message_channel(channel, message)
 {
-	const config = get_config('github').bitrix;
+	const config = get_config('bitrix');
 	const url = `${config.url}/${config.id}/${config.token}/im.message.add`;
-	const channel_id = await get_channel_id(channel);
+	const channel_id = await get_channel_id(config, channel).catch(err => console.error(err));
 
 	if (channel_id)
-		return await axios.post(url, `CHAT_ID=${channel_id}&MESSAGE=${message}`, config);
+		return await axios.post(url, `CHAT_ID=${channel_id}&MESSAGE=${message}`).catch(err => console.error(err));
 }
 
 exports.send_message_user = async function send_message_user(email, message)
 {
-	const config = get_config('github').bitrix;
+	const config = get_config('bitrix');
 	const url = `${config.url}/${config.id}/${config.token}/im.message.add`;
-	const channel_id = await get_user_id(email);
+	const user_id = await get_user_id(config, email);
 
-	if (channel_id)
-		return await axios.post(url, `USER_ID=${channel_id}&MESSAGE=${message}`, config);
+	if (user_id)
+		return await axios.post(url, `USER_ID=${user_id}&MESSAGE=${message}`).catch(err => console.error(err));
 }
